27593cab36b5d77f87c19e48e12807758b3e2611
Edson Fernandez <edson.fo@gmail.com>
2021-03-17 15:15:27 -0400

    project setup TOD-01

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -2,11 +2,7 @@ import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
-  const wrapper = shallowMount(HelloWorld, {
-    mocks: {
-      $t: msg => msg
-    }
-  })
+  const wrapper = shallowMount(HelloWorld)
   it('should match snapshot', () => {
     expect(wrapper.element).toMatchSnapshot()
   })
